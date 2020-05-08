ef43a9f72c50da6177bfdd1415e2b1de699ec947
Luke Trujillo <lrtrujillo@wpi.edu>
2020-05-08 13:26:59 -0400

    Final commit before submission

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 import data.Record;
 import dataset.DataFileReader;
 import dataset.Dataset;
@@ -7,12 +9,37 @@ import dataset.Dataset;
 public class Main {
 
 	public static void main(String[] args) {
-	
-		Query.performHashJoin();
-	
-		Query.performBlockLevelNestedLoopJoin();
 		
-		Query.performHashBasedAggregationFunction(Dataset.A,AggregationFunctionType.SUM_RANDOMV);
+		
+		Scanner scanner = new Scanner(System.in);
+	
+		while(true) {
+			System.out.println("Enter query:");
+			String input = scanner.nextLine();
+			
+			switch(input) {
+			case "SELECT A.Col1, A.Col2, B.Col1, B.Col2 FROM A, B WHERE A.RandomV = B.RandomV":
+				Query.performHashJoin();
+				break;
+			case "SELECT count(*) FROM A, B WHERE A.RandomV > B.RandomV":
+				Query.performBlockLevelNestedLoopJoin();
+				break;
+			case "SELECT Col2, SUM(RandomV) FROM A GROUP BY Col2":
+				Query.performHashBasedAggregationFunction(Dataset.A,AggregationFunctionType.SUM_RANDOMV);
+				break;
+			case "SELECT Col2, SUM(RandomV) FROM B GROUP BY Col2":
+				Query.performHashBasedAggregationFunction(Dataset.B,AggregationFunctionType.SUM_RANDOMV);
+				break;	
+			case "SELECT Col2, AVG(RandomV) FROM A GROUP BY Col2":
+				Query.performHashBasedAggregationFunction(Dataset.A,AggregationFunctionType.AVG_RANDOMV);
+				break;
+			case "SELECT Col2, AVG(RandomV) FROM B GROUP BY Col2":
+				Query.performHashBasedAggregationFunction(Dataset.B,AggregationFunctionType.AVG_RANDOMV);
+				break;	
+			}
+			
+		}
+		
 	}
 
 }
